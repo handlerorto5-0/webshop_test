0f3b45e41c5dbcab12846710ba7d40ee4cd8aef4
kv-2 <str_token@mail.ru>
2019-07-13 23:44:38 +0300

    seeds integration test

--- test/integration/invalid_new_user_workflow_test.rb
+++ test/integration/invalid_new_user_workflow_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class InvalidNewUserWorkflowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all 
+
+  test "try create a new user without a login" do
+    @user = users(:one)
+
+    get '/users/new'
+    assert_response :success
+
+    post users_url, params: { user: {last_name: @user.last_name}}
+    assert_equal '/users', path
+    assert_select 'li', "Login name can't be blank"
+   # assert_select 'li', "Login name too short (minimum is 10 characters)"
+  end
 end
