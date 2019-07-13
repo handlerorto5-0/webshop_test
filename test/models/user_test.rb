89a156334d1915772eeaabf7de8a4a52f0870986
kv-2 <str_token@mail.ru>
2019-07-13 22:53:48 +0300

    add unit test
    unit only by test/model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,9 @@ class UserTest < ActiveSupport::TestCase
     user = User.new
     assert_not user.save, 'Saved a user with no attributes'
   end
+
+  test "the two fixture users are valid" do
+    assert User.new(last_name: users(:one).last_name, login_name: users(:one).login_name ).valid?, 'First fixture is not valid.'
+    assert User.new(last_name: users(:two).last_name, login_name: users(:two).login_name ).valid?, 'Second fixture is not valid.'
+  end
 end
